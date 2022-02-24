c8b3b943d50f30254f6459b4e057ad766bf8e704
CeritaMustofa <mustofa@mustofa.pinuin>
2022-02-24 03:11:30 -0800

    edit init auth model

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,21 +1,24 @@
 import os
+from datetime import timedelta
 from flask import Flask
 from flaskr.model import db
 from flask_migrate import Migrate
-from flaskr.auth import auth
+from flaskr.auth import auth, jwt
+from flaskr.dashboard import dashboard
 from flask_jwt_extended import JWTManager
 
 def create_app(test_config=None):
     #create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    
+    ACCESS_EXPIRES = timedelta(hours=1)
     
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
-            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY")
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
+            JWT_ACCESS_TOKEN_EXPIRES = ACCESS_EXPIRES
         )
     else:
         # Load the test config if passed in
@@ -27,7 +30,7 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate = Migrate(app, db)
     
-    JWTManager(app)
+    jwt.init_app(app)
     
     try:
         os.makedirs(app.instance_path)
@@ -36,6 +39,7 @@ def create_app(test_config=None):
     
     
     app.register_blueprint(auth)
+    app.register_blueprint(dashboard)
     
     
     return app
